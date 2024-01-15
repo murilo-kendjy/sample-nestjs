f9e6fa0d6d57f962e4d34db5f4c9be4d4bc62947
Murilo Kendjy Vieira Onita <muriloonita@gmail.com>
2024-01-15 15:48:38 -0300

    feat: Add not found interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { DrizzleModule } from './drizzle/drizzle.module.js';
 import { AuthGuard } from './guards/auth/auth.guard.js';
 import { RolesGuard } from './guards/roles/roles.guard.js';
+import { NotFoundInterceptor } from './interceptors/not-found/not-found.interceptor.js';
 import { LoggingMiddleware } from './middlewares/logging/logging.middleware.js';
 import { AuthModule } from './resources/auth/auth.module.js';
 import { UsersModule } from './resources/users/users.module.js';
@@ -21,6 +22,10 @@ import { UsersModule } from './resources/users/users.module.js';
     {
       provide: APP_GUARD,
       useClass: RolesGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: NotFoundInterceptor,
     },
   ],
 })
---
+++ src/interceptors/not-found/not-found.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+  data: T;
+}
+
+@Injectable()
+export class NotFoundInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => {
+        if (!data) {
+          throw new HttpException(
+            'Nenhum resultado encontrado',
+            HttpStatus.NOT_FOUND,
+          );
+        }
+
+        return data;
+      }),
+    );
+  }
+}
